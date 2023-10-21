98b4601fee80d8297008b9dd5113be88112a2291
rezajavadidey <77723819+rezajavadidey@users.noreply.github.com>
2023-10-21 14:05:14 +0330

    edit

--- WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
@@ -10,11 +10,14 @@ namespace WebApplication1.Controllers
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", 
             "Sweltering",
+
+
             "Scorching"
 
 
 
     };
+
 
         private readonly ILogger<WeatherForecastController> _logger;
 
